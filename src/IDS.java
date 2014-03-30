5ea9e1736660be5418e86900a74b48fff0ddf9b5
Matt Prpic <mattprpic@live.ca>
2014-03-30 18:49:04 -0500

    Merged Algorithm and GUI

--- src/IDS.java
+++ src/IDS.java
@@ -4,10 +4,14 @@ import java.awt.Rectangle;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.SocketException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -17,13 +21,11 @@ import javax.swing.text.DefaultCaret;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashMap;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.util.List;
+
 import org.jnetpcap.Pcap;
 import org.jnetpcap.PcapIf;
 import org.jnetpcap.packet.JPacket;
@@ -31,6 +33,7 @@ import org.jnetpcap.packet.JPacketHandler;
 import org.jnetpcap.packet.format.FormatUtils;
 import org.jnetpcap.protocol.network.Ip4;
 import org.jnetpcap.protocol.tcpip.Udp;
+
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 
@@ -43,7 +46,7 @@ public class IDS extends JFrame implements ActionListener {
 	private JPanel contentPane;
 	private JButton scanButton;
 	private JTextArea textArea;
-	private JComboBox<String> comboBox;
+	private JComboBox comboBox;
 
 	// Stores all interface devices
 	List<PcapIf> alldevs = new ArrayList<PcapIf>();
@@ -91,7 +94,6 @@ public class IDS extends JFrame implements ActionListener {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-<<<<<<< HEAD
 		textArea = new JTextArea();
 		textArea.setEditable(false);
 		textArea.setBounds(32, 39, 500, 450);
@@ -102,23 +104,18 @@ public class IDS extends JFrame implements ActionListener {
 		scanButton.addActionListener(this);
 		contentPane.add(scanButton);
 
-		comboBox = new JComboBox<String>();
+		comboBox = new JComboBox();
 		comboBox.setBounds(544, 88, 198, 25);
 		contentPane.add(comboBox);
-=======
-	@SuppressWarnings("deprecation")
-	public static void main(String[] args) throws IOException {
-		//Stores all interface devices
-		List<PcapIf> alldevs = new ArrayList<PcapIf>(); 
->>>>>>> 2723f233979808f220c80d5379aa4636485efdc7
-		
-		DefaultCaret caret = (DefaultCaret)textArea.getCaret();
+
+		DefaultCaret caret = (DefaultCaret) textArea.getCaret();
 		caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
 		textArea.setWrapStyleWord(true);
 		textArea.setCaretPosition(textArea.getDocument().getLength());
-		
-		JScrollPane scroll = new JScrollPane (textArea,
-		JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
+		JScrollPane scroll = new JScrollPane(textArea,
+				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		scroll.setSize(500, 450);
 		scroll.setLocation(20, 27);
 		contentPane.add(scroll);
@@ -150,15 +147,14 @@ public class IDS extends JFrame implements ActionListener {
 
 	}
 
-	public void runIDS(int d) throws SocketException {
-		
+	public void runIDS(int d) throws IOException {
+
 		final PcapIf device = alldevs.get(d);
 		// TODO Doesn't dynamically update the JTextArea
 		textArea.append("\nChoosing '"
 				+ ((device.getDescription() != null) ? device.getDescription()
 						: device.getName()) + "':\n");
 
-		
 		// Finds the LAN IP address of the host from network interfaces
 		Enumeration<NetworkInterface> n = NetworkInterface
 				.getNetworkInterfaces();
@@ -176,39 +172,35 @@ public class IDS extends JFrame implements ActionListener {
 					textArea.append("\nHost address: " + localAddr + "\n");
 				}
 
-			} 		
+			}
 
 		}
 
 		// Stores the LAN IP address of the host.
 		final String hostAddr = localAddr;
 
-<<<<<<< HEAD
-		// Packet capturing settings
-=======
-		//Retrieve default gateway address
-		String[] cmd = {"/bin/sh","-c","netstat -rn | grep 0.0.0.0 | awk \'{print $2}\' | grep -v \"0.0.0.0\""};
+		// Retrieve default gateway address
+		String[] cmd = { "/bin/sh", "-c",
+				"netstat -rn | grep 0.0.0.0 | awk \'{print $2}\' | grep -v \"0.0.0.0\"" };
 		Process result = Runtime.getRuntime().exec(cmd);
-	    BufferedReader output = new BufferedReader(new InputStreamReader(result.getInputStream()));
-	    final String defaultGate = output.readLine();
-	    System.out.printf("Default Gateway: %s\n\n", defaultGate);
+		BufferedReader output = new BufferedReader(new InputStreamReader(
+				result.getInputStream()));
+		final String defaultGate = output.readLine();
+		textArea.append("Default Gateway: " + defaultGate + "\n");
 
-	    
-		//Packet capturing settings
->>>>>>> 2723f233979808f220c80d5379aa4636485efdc7
+		// Packet capturing settings
 		int snaplen = 64 * 1024;
 		int flags = Pcap.MODE_PROMISCUOUS;
 		int timeout = 10 * 1000;
 		// Open capturing channel
-		Pcap pcap = Pcap.openLive(device.getName(), snaplen, flags, timeout,
-				errbuf);
+		final Pcap pcap = Pcap.openLive(device.getName(), snaplen, flags,
+				timeout, errbuf);
 
 		if (pcap == null) {
 			textArea.append("Error while opening device for capture: "
 					+ errbuf.toString());
 			return;
 		}
-<<<<<<< HEAD
 
 		// Main handler when a packet is captured
 		JPacketHandler<String> jpacketHandler = new JPacketHandler<String>() {
@@ -217,31 +209,21 @@ public class IDS extends JFrame implements ActionListener {
 			// Tcp tcp = new Tcp();
 			int counter = 0;
 
+			boolean exitFlag = false;
+
 			// Stores source IPs and their corresponding data sent to the host
 			// (in bytes)
 			HashMap<String, Integer> sources = new HashMap<String, Integer>();
+			HashMap<String, Long> startTimes = new HashMap<String, Long>();
+
+			long bpsLimit = 60000;
 
 			public void nextPacket(JPacket packet, String user) {
 
 				// Holds the source and destination IP addresses
-=======
-		
-		//Main handler when a packet is captured
-		JPacketHandler<String> jpacketHandler = new JPacketHandler<String>() {  
-		    Udp udp = new Udp();
-		    Ip4 ip = new Ip4();
-		    //Tcp tcp = new Tcp();
-		    int counter = 0;
-		    
-		    //Stores source IPs and their corresponding data sent to the host (in bytes)
-		    HashMap<String, Integer> sources = new HashMap<String, Integer>();
-		    
-		    public void nextPacket(JPacket packet, String user) {  
-		    	
-		    	//The source and destination IP addresses
->>>>>>> 2723f233979808f220c80d5379aa4636485efdc7
 				byte[] sIP = new byte[4];
 				byte[] dIP = new byte[4];
+				double bps = 0;
 
 				if (!(packet.hasHeader(ip))) {
 					return;
@@ -257,17 +239,26 @@ public class IDS extends JFrame implements ActionListener {
 				String sourceIP = FormatUtils.ip(sIP);
 				String destinationIP = FormatUtils.ip(dIP);
 
-<<<<<<< HEAD
 				// Displays the packet information such as source and
 				// destination IP addresses along with ports and the size of
 				// each packet in bytes.
 				if ((packet.hasHeader(udp)) && (packet.hasHeader(ip))) {
 
 					// Filters out packets sent by the current host.
-					if (!(sourceIP.equals(hostAddr))) {
+					if (destinationIP.equals(hostAddr)) {
 
 						// Add source IP to the map
 						if (!(sources.containsKey(sourceIP))) {
+							sources.put(sourceIP, 0);
+							startTimes
+									.put(sourceIP, System.currentTimeMillis());
+						}
+
+						// Refresh start times
+						if (((System.currentTimeMillis() - startTimes
+								.get(sourceIP)) / 60000) > 1) {
+							startTimes
+									.put(sourceIP, System.currentTimeMillis());
 							sources.put(sourceIP, 0);
 						}
 
@@ -276,12 +267,23 @@ public class IDS extends JFrame implements ActionListener {
 						sources.put(sourceIP,
 								sources.get(sourceIP) + packet.size());
 						// TODO Doesn't dynamically update the JTextArea
-						textArea.append("Found UDP packet, source "
+						textArea.append("Incoming UDP packet, source: "
 								+ sourceIP + ":" + udp.source() + " "
-								+ "destination" + destinationIP + ":"
+								+ "destination: " + destinationIP + ":"
 								+ udp.destination() + " size " + packet.size()
 								+ "\n");
-												
+						if (((System.currentTimeMillis() - startTimes
+								.get(sourceIP)) / 1000) >= 10) {
+							bps = (double) (sources.get(sourceIP) / (double) ((System
+									.currentTimeMillis() - startTimes
+									.get(sourceIP)) / 1000));
+						}
+
+						if (bps > bpsLimit) {
+							JOptionPane.showMessageDialog(null,
+									"UDP Flood from: " + sourceIP);
+							exitFlag = true;
+						}
 
 						// For every 50 packets received print the hashmap of
 						// all source IPs and their corresponding data (in
@@ -291,35 +293,13 @@ public class IDS extends JFrame implements ActionListener {
 							counter = 0;
 							textArea.update(textArea.getGraphics());
 						}
+
+						if (exitFlag) {
+							pcap.breakloop();
+						}
 					}
 				}
-			} 
-=======
-				//Displays the packet information such as source and destination IP addresses along with ports and the size of each packet in bytes.
-		    	if((packet.hasHeader(udp)) && (packet.hasHeader(ip))) {  
-		    		
-		    		//Filters out unwanted packet information.
-		    		if (!(sourceIP.equals(hostAddr)) && (!(sourceIP.equals(defaultGate))) && (destinationIP.equals(hostAddr))){
-		    			
-		    			//Add source IP to the map
-		    			if (!(sources.containsKey(sourceIP))){
-		    				sources.put(sourceIP, 0);
-		    			}
-		    			
-		    			//Update the total received data for the corresponding source IP
-		    			sources.put(sourceIP, sources.get(sourceIP) + packet.size());
-		    			System.out.printf("Found UDP packet, source %s:%d destination %s:%d size %d\n", sourceIP, udp.source(), destinationIP, udp.destination(), packet.size());
-		    			
-		    			//For every 50 packets received print the hashmap of all source IPs and their corresponding data (in bytes)
-		    			if (counter++ == 50){
-		    				System.out.println(sources.toString());
-		    				counter = 0;
-		    			}
-		    		}
-		        }  
-		    }
- 
->>>>>>> 2723f233979808f220c80d5379aa4636485efdc7
+			}
 		};
 
 		// Packet capturing loop that currently does not end until termination
@@ -329,31 +309,35 @@ public class IDS extends JFrame implements ActionListener {
 		pcap.close();
 
 	}
-	
-	class ValidateThread implements Runnable{
+
+	class ValidateThread implements Runnable {
 
 		@Override
 		public void run() {
 			// TODO Auto-generated method stub
-			
-			// Get the users choice from the drop down list of network interfaces
+
+			// Get the users choice from the drop down list of network
+			// interfaces
 			int choice = comboBox.getSelectedIndex();
-			
+
 			try {
 				runIDS(choice);
 			} catch (SocketException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			
+
 		}
-		
+
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 
-		if (arg0.getSource() == scanButton){
+		if (arg0.getSource() == scanButton) {
 			Runnable runnable = new ValidateThread();
 			Thread thread = new Thread(runnable);
 			thread.start();
